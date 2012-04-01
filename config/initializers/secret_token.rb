b8fefb4d06121323a21275ded2572f0539f52305
Paul Dixon <pd@harnu.com>
2012-04-01 15:41:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '888db7a53c1534678e45fcf7b0e0143b531c4bd175c7342fde60058d4b9d3e7328572216b2e1b747f1c34cc3b9741c65001eb7342901549429459a64e52f0f3d'
